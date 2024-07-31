e84330dc1bc1f66ccdf7dd555d72e2be0f275837
Emmanuel Nnamdi <engr.nnamdiemmanuel@gmail.com>
2024-07-31 17:06:43 +0100

    updated terra files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,37 @@
 # deploy the k8s cluster via terraform
 
 provider "aws" {
-  region = "us-east-2" 
+  region = "us-east-2"
 }
 
+
+resource "aws_launch_template" "eks" {
+  name_prefix   = "eks-"
+  image_id      = data.aws_ami.latest-ubuntu-jammy-22-04-image.id
+  instance_type = "t2.medium"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+
+data "aws_ami" "latest-ubuntu-jammy-22-04-image" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+
+
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "3.11.0"
+  source = "git::https://github.com/IKUKU1010/terraform-aws-vpc.git?ref=master"
+
 
   name = "socks-shop-vpc"
   cidr = "10.0.0.0/16"
@@ -14,13 +39,11 @@ module "vpc" {
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+  enable_dns_hostnames = true
 }
-
-
-enable_nat_gateway = true
-
-single_nat_gateway = true
-enable_dns_hostnames = true
 
 
 module "eks" {
@@ -32,44 +55,38 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    # Use Ubuntu AMI
-    ami_type = "CUSTOM"
-  }
-
-  eks_managed_node_group = {
+  eks_managed_node_groups = {
     work-node1 = {
-      name    = "sockapp-node1"
-      ami = data.aws_ami.latest-ubuntu-noble-24-04-image.id
+      name           = "sockapp-node1"
+      launch_template = {
+        id      = aws_launch_template.eks.id
+        version = "$Latest"
+      }
       instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 2
       desired_size = 2
     }
-
-
 
     work-node2 = {
-         
-      name    = "sockapp-node2"
-      ami = data.aws_ami.latest-ubuntu-noble-24-04-image.id
+      name           = "sockapp-node2"
+      launch_template = {
+        id      = aws_launch_template.eks.id
+        version = "$Latest"
+      }
       instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 2
       desired_size = 2
     }
-
-
   }
 
-  # Cluster access entry
+   # Cluster access entry
   # To add the current caller identity as an administrator
   enable_cluster_creator_admin_permissions = true
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,7 +14,3 @@ output "eks_cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
 
-
- output "eks_managed_node_group_ami_id" = {
-    value = data.aws_ami.latest-ubuntu-noble-24-04-image.id
-  }
